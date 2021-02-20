c6112542e3c66b466db1e0f04699bf805c7bf56b
Palash Ahmed <49250151+palahsu@users.noreply.github.com>
2021-02-20 23:04:47 +0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='bomberwhole',
+    packages=find_packages(),
+    include_package_data=True,
+    version='1.0.0',
+    author='Palahsu',
+    author_email='jk3502072@gmail.com',
+    url='https://github.com/palahsu/MBomb',
+    download_url='https://github.com/palahsu/MBomb.git',
+    description="Presenting BomberWhole to Unlimited Messages Any Social Media!",
+    long_description=long_description,
+    license="LGPLv3",
+    long_description_content_type="text/markdown",
+    keywords=["mbomb", "mail-bomb", "mail-bombing", "palahsu", "bombing", 
+    "exploit", "tool", "unlimited-message", "message", "hacker-scripts", "hacking-tool", "hacking", "vulnerability", "bomber",
+    "cybersecurity", "cyber-security", "information-security", "security"
+              "messages"],
+    install_requires=[
+        'os'
+        'selenium'
+        'webdriver'
+        'ChromeDriverManager'
+        
+    ],
+    classifiers=[
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+],
+
+)
